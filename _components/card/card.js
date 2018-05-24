af877e1e7075ce3e4756ce14146e47ec102d53fb
vladimir dashukevich <dashukevichvova@gmail.com>
2018-05-25 01:32:22 +0300

    Added more presentations with videos. Fixed style issues connected with positioning presentation cards.

--- _components/card/card.js
+++ _components/card/card.js
@@ -4,22 +4,25 @@
         this.defImg = "/algorithms/pictures/1.png";
     };
 
-    Card.prototype.videoToLink = function (video, index) {
-        return "<a class='presentation-card__video-link' href='" + video + "'>Video " + (index + 1) + "</a>";
+    Card.prototype.videoToLink = function (video, title) {
+        return "<a target='_blank' class='presentation-card__video-link' href='" + video + "'>" + title +"</a>";
     };
 
     Card.prototype.videosToLink = function () {
         if (this.options.videos) {
             var videos = this.options.videos;
-            return "<div class='presentation-card__video-list'>" + videos.map(this.videoToLink).join(" | ") + "</div>";
+            var addIndex = videos.length > 1;
+            return "<div class='presentation-card__video-list'>" + videos.map(function (video, index) {
+                return this.videoToLink(video, addIndex ? "Video " + (index + 1) : "Video");
+            }.bind(this)).join(" | ") + "</div>";
         }
 
         return "";
     };
 
     Card.prototype.toHTML = function () {
-        return "<div class='presentation-card'>" +
-                    "<a class='presentation-card__link' href='" + this.options.link + "'>" +
+        return "<div class='presentation-card presentations-document__list-item'>" +
+                    "<a target='_blank' class='presentation-card__link' href='" + this.options.link + "'>" +
                         "<div class='presentation-card__header'>" +
                             "<h3 class='presentation-card__title'>" + this.options.title + "</h3>" +
                         "</div>" +
--- _scripts/presentations.js
+++ _scripts/presentations.js
@@ -4,6 +4,19 @@
         img: "/algorithms/pictures/25.png",
         title: "Algorithms and data structures that change frontend",
         videos: ["https://www.youtube.com/watch?v=nocJsLFPjNk"]
+    }, {
+        link: "/quantum",
+        img: "/quantum/pictures/4.jpg",
+        title: "WASM vs Quantum computer",
+        videos: ["https://www.youtube.com/watch?v=4_lVq1-lr8g"]
+    }, {
+        link: "/tls",
+        img: "/tls/pictures/4.jpg",
+        title: "TLS/SSL",
+        videos: [
+            "https://www.youtube.com/watch?v=IOBorSRbGYs",
+            "https://www.youtube.com/watch?v=VufUKONzZdM"
+        ]
     }];
 
     w.Presentations = presentations;
