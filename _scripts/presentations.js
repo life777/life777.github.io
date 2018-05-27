833f64b2f8960e093f06353726707d999f374a86
vladimir dashukevich <dashukevichvova@gmail.com>
2018-05-28 00:06:01 +0300

    Added all web presentations

--- _scripts/presentations.js
+++ _scripts/presentations.js
@@ -18,12 +18,115 @@
             "https://www.youtube.com/watch?v=VufUKONzZdM"
         ]
     }, {
+        link: "/ts",
+        img: "/ts/pictures/3.gif",
+        title: "Fight bugs in JavaScript",
+        videos: [
+            "https://www.youtube.com/watch?v=_B6N_7tF6MA"
+        ]
+    }, {
         link: "/testing",
         img: "/testing/pictures/8.jpg",
         title: "Unit tests in JS",
         videos: [
             "https://www.youtube.com/watch?v=9qSZeVSa7ts"
         ]
+    }, {
+        link: "/https",
+        img: "/https/pictures/9.png",
+        title: "HTTP vs HTTPS",
+        videos: [
+            "https://www.youtube.com/watch?v=IOBorSRbGYs"
+        ]
+    }, {
+        link: "/CSP",
+        img: "/CSP/pictures/48.png",
+        title: "Content Security Policy",
+        videos: [
+            "https://www.youtube.com/watch?v=5T6OnChCO78"
+        ]
+    }, {
+        link: "/rust",
+        img: "/rust/pictures/1.png",
+        title: "Rust language",
+        videos: [
+            "https://www.youtube.com/watch?v=2gIivSjhYOc"
+        ]
+    }, {
+        link: "/webComponents",
+        img: "/webComponents/pictures/1.png",
+        title: "Web components",
+        videos: [
+            "https://www.youtube.com/watch?v=sH04-Ypak_s"
+        ]
+    }, {
+        link: "/future2",
+        img: "/future2/pictures/2.jpg",
+        title: "Future of Javascript",
+        videos: [
+            "https://www.youtube.com/watch?v=_OcnHy64xM0"
+        ]
+    }, {
+        link: "/security",
+        img: "/security/pictures/47.jpg",
+        title: "Threats in frontend",
+        videos: [
+            "https://www.youtube.com/watch?v=HYVXxeSjPK8",
+            "https://www.youtube.com/watch?v=h9iQMmjS5ec"
+        ]
+    }, {
+        link: "/amp",
+        img: "/amp/pictures/1.jpeg",
+        title: "AMP pages",
+        videos: [
+            "https://www.youtube.com/watch?v=yamUsRCBqyk"
+        ]
+    }, {
+        link: "/wsd",
+        img: "/wsd/pictures/11.jpg",
+        title: "Transpilers to JavaScript",
+        videos: [
+            "https://youtu.be/UPtDcAbg6EI?t=6h51m50s"
+        ]
+    }, {
+        link: "/promises",
+        img: "/promises/pictures/4.gif",
+        title: "Promises in JavaScript",
+        videos: [
+            "https://www.youtube.com/watch?v=M1N6WlJEEMs"
+        ]
+    }, {
+        link: "/speech",
+        img: "/speech/pictures/speech7.jpg",
+        title: "Speech recognition",
+        videos: [
+            "https://www.youtube.com/watch?v=9FXTj4jt2T4"
+        ]
+    }, {
+        link: "/http",
+        img: "/http/pictures/20.png",
+        title: "HTTP2",
+        videos: [
+            "https://www.youtube.com/watch?v=iZI-Pt6ntu0"
+        ]
+    }, {
+        link: "/vsRuby",
+        img: "/vsRuby/pictures/16.jpg",
+        title: "NodeJS vs Ruby",
+        videos: [
+            "https://www.youtube.com/watch?v=HDpIc8QCJTw",
+            "https://www.youtube.com/watch?v=SpFaCSyIy5Q"
+        ]
+    }, {
+        link: "/es6",
+        img: "/es6/pictures/2.jpg",
+        title: "ES 2015",
+        videos: []
+    }, {
+        link: "/wasm",
+        img: "/wasm/pictures/1.png",
+        title: "WebAssembly",
+        videos: []
     }];
 
     w.Presentations = presentations;
